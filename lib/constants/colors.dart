bb70db39ce2935912cc7dedb8b3d196bf2fe27c7
Max All <maxall32025@gmail.com>
2025-05-06 23:03:50 +0300

    تعديل جديد

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,10 +2,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xFF4B0082); // اللون الأساسي (أرجواني داكن مثل STC)
+  static const primary =
+      Color(0xFF4B0082); // اللون الأساسي (أرجواني داكن مثل STC)
   static const secondary = Color(0xFFFFA500); // لون مكمل (برتقالي فاتح)
-  static const background = Color(0xFFF5F5F5); // لون الخلفية (أبيض مائل للرمادي)
+  static const background =
+      Color(0xFFF5F5F5); // لون الخلفية (أبيض مائل للرمادي)
   static const textPrimary = Color(0xFF333333); // لون النص الأساسي (أسود مخفف)
   static const textSecondary = Color(0xFF777777); // لون النص الثانوي (رمادي)
   static const buttonText = Colors.white; // لون النص على الأزرار (أبيض)
+  static const test = Colors.white; // لون النص على الأزرار (أبيض)
 }
